de6f12bdd4d28990bad6fb9a7f5ae55a235133b0
Domenico Capalbo <capalbodomenico@gmail.com>
2021-01-17 23:41:43 +0100

    from-array-of-numbers-make-sum

--- snack-9/js/script.js
+++ snack-9/js/script.js
@@ -1,12 +1,22 @@
 // Chiedi un numero di 4 cifre all’utente e calcola la somma
 // di tutte le cifre che compongono il numero.
 
-let numeroUtente = prompt("inserisci un numero a quattro cifre");
-
-let sumTotal = 0;
-
-for (let i = 0; i < numeroUtente.length; i++) {
-  sumTotal += parseInt(numeroUtente[i]);
-}
-
+// Arrays 
+let numbers = [3, 45, 10, 23, 9, 45, 34];
+// constant reducer function to sum the value inside of the array 
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+let sumTotal = numbers.reduce(reducer);
 console.log(sumTotal);
+
+function sumDigits(sumTotal) {
+    subTotal = 0;
+    let i = 0;
+    while (sumTotal) {
+        subTotal += sumTotal % 10;
+        sumTotal = Math.floor(sumTotal / 10);
+        i++
+    }
+}
+sumDigits(sumTotal);
+console.log(subTotal);
+
